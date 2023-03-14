aa3889a25cf2e91686ca69c985965d0656935bfc
Jluzano <justinluzano@gmail.com>
2023-03-14 00:16:14 -0700

    test

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from PIL import ImageTk,Image
 import json
 
 def left(event):
-    ImageTk.PhotoImage(Image.open(map["(0, 0)"]["left"]))
+    currentDir = map[currentPos]["left"]
+    ImageTk.PhotoImage(Image.open(currentDir))
 
+currentDir = "north"
+currentPos = "(0, 0)"
 m = open("map.json")
 map = json.load(m)
 root = Tk()
-myImg = ImageTk.PhotoImage(Image.open(map["(0, 0)"]["north"]["IMG"]))
+myImg = ImageTk.PhotoImage(Image.open(map[currentPos][currentDir]["IMG"]))
 myLabel = Label(image = myImg)
 myLabel.pack()
 root.bind("<Left>", left)
+'''
 root.bind("<Right>", right)
 root.bind("<Up>", up)
 root.bind("<Down>", down)
+'''
 root.mainloop()
