005fa9f2999740b4d560422e5b20f119335477e4
Jluzano <justinluzano@gmail.com>
2023-03-14 00:04:37 -0700

    testing turning

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from tkinter import *
 from PIL import ImageTk,Image
 import json
 
+def left(event):
+    ImageTk.PhotoImage(Image.open(map["(0, 0)"]["left"]))
+
 m = open("map.json")
 map = json.load(m)
 root = Tk()
 myImg = ImageTk.PhotoImage(Image.open(map["(0, 0)"]["north"]["IMG"]))
 myLabel = Label(image = myImg)
 myLabel.pack()
+root.bind("<Left>", left)
+root.bind("<Right>", right)
+root.bind("<Up>", up)
+root.bind("<Down>", down)
 root.mainloop()
