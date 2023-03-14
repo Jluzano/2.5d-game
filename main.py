1e9777ebeade423112fa3c646ddd415d59a7663b
Jluzano <justinluzano@gmail.com>
2023-03-13 22:58:27 -0700

    testing json file

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from tkinter import *
 from PIL import ImageTk,Image
+import json
 
+m = open("map.json")
+map = json.load(m)
 root = Tk()
-myImg = ImageTk.PhotoImage(Image.open("horse.PNG"))
+myImg = ImageTk.PhotoImage(Image.open(map["(0, 0)"]["north"]["IMG"]))
 myLabel = Label(image = myImg)
 myLabel.pack()
 root.mainloop()
