8ba194ca91f2662833285b5fdd5a6aa2e87aadaa
Jluzano <justinluzano@gmail.com>
2023-03-15 02:15:20 -0700

    test

--- main.py
+++ main.py
@@ -141,13 +141,13 @@ resizeMap = mapImg.resize((80, 80), Image.ANTIALIAS)
 newMap = ImageTk.PhotoImage(resizeMap)
 mapLabel = Label(image = newMap, anchor = NE)
 
-# Placing the compass on top of the background image
-compassLabel.place(x=0, y=0)
-mapLabel.place(relx=1, y=0, anchor='ne')
-
 # This line resizes the window to match the height
 # and width of the background image so you don't have to resize
 root.geometry('{}x{}'.format(img2.width(), img2.height()))
+
+# Placing the compass on top of the background image
+compassLabel.place(x=0, y=0)
+mapLabel.place(relx=1, y=0, anchor='ne')
 
 # Binding the arrow keys to functions initialized earlier
 root.bind("<Left>", left)
