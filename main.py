f718a7614fa22752009ed43eec1cba6210380f36
Jluzano <justinluzano@gmail.com>
2023-03-14 20:46:09 -0700

    more compass shenanigans

--- main.py
+++ main.py
@@ -4,40 +4,65 @@ import json
 
 currentDir = "north"
 currentPos = "(0, 0)"
+def changeCompass(currentDir):
+    global compassLabel
+    if currentDir == "north":
+        updateCompass = Image.open("imgs/compass/faceNorth.png")
+        compassLabel = Label(image = updateCompass)
+    elif currentDir == "east":
+        updateCompass = Image.open("imgs/compass/faceEast.png")
+        compassLabel = Label(image = updateCompass)
+    elif currentDir == "south":
+        updateCompass = Image.open("imgs/compass/faceSouth.png")
+        compassLabel = Label(image = updateCompass)
+    elif currentDir == "west":
+        updateCompass = Image.open("imgs/compass/faceWest.png")
+        compassLabel = Label(image = updateCompass)
+
+# Function to replace the current image with the next image
 def replace(currentDir, currentPos):
     img = ImageTk.PhotoImage(Image.open(map[currentPos][currentDir]["IMG"]))
     myLabel.configure(image=img)
     myLabel.image = img
 
+# Function 
 def left(event):
     global currentDir, myLabel
     if currentDir == "north":
         currentDir = "west"
         replace(currentDir, currentPos)
+        changeCompass(currentDir)
     elif currentDir == "east":
         currentDir = "north"
         replace(currentDir, currentPos)
+        changeCompass(currentDir)
     elif currentDir == "south":
         currentDir = "east"
         replace(currentDir, currentPos)
+        changeCompass(currentDir)
     elif currentDir == "west":
         currentDir = "south"
         replace(currentDir, currentPos)
+        changeCompass(currentDir)
 
 def right(event):
     global currentDir, myLabel
     if currentDir == "north":
         currentDir = "east"
         replace(currentDir, currentPos)
+        changeCompass(currentDir)
     elif currentDir == "east":
         currentDir = "south"
         replace(currentDir, currentPos)
+        changeCompass(currentDir)
     elif currentDir == "south":
         currentDir = "west"
         replace(currentDir, currentPos)
+        changeCompass(currentDir)
     elif currentDir == "west":
         currentDir = "north"
         replace(currentDir, currentPos)
+        changeCompass(currentDir)
 
 def up(event):
     global currentDir, myLabel, currentPos
@@ -68,21 +93,24 @@ def down(event):
         myLabel.image = img
     else:
         print("Can't go that way.")
-
+# Initializing the map json file
 m = open("map.json")
 map = json.load(m)
+# Initializing the background image
 root = Tk()
 myImg = ImageTk.PhotoImage(Image.open(map[currentPos][currentDir]["IMG"]))
 myLabel = Label(image = myImg)
-
+# Initiallizing the compass
 compassImg = Image.open("imgs/compass/faceNorth.png")
 resizeCompass = compassImg.resize((50, 50), Image.ANTIALIAS)
 newCompass = ImageTk.PhotoImage(resizeCompass)
 compassLabel = Label(image = newCompass)
-
+# Placing the compass on top of the background image
 myLabel.place(x=0, y=0)
 compassLabel.place(x=0, y=0)
+# This line resizes the window to match the height and width of the background image
 root.geometry('{}x{}'.format(myImg.width(), myImg.height()))
+# Binding the arrow keys to functions initialized earlier
 root.bind("<Left>", left)
 root.bind("<Right>", right)
 root.bind("<Up>", up)
