126df5cba1a5fdce9ff5280717dc1aff37bbdd90
Jluzano <justinluzano@gmail.com>
2023-03-19 10:22:07 -0700

    changing some more resizing lines

--- main.py
+++ main.py
@@ -124,11 +124,10 @@ map = json.load(m)
 # Initializing the background image
 root = Tk()
 img = Image.open(map[currentPos][currentDir]["IMG"])
-img2 = img.resize((1344, 653), Image.Resampling.LANCZOS)
-img3 = ImageTk.PhotoImage(img2)
-canvas = Canvas(root, width = 500, height = 500)
+img2 = ImageTk.PhotoImage(img)
+canvas = Canvas(root, width = 1344, height = 653)
 canvas.pack(fill="both", expand=TRUE)
-canvas.create_image(0, 0, image = img3, anchor="nw")
+canvas.create_image(0, 0, image = img2, anchor="nw")
 
 # Initiallizing the compass, you start facing north
 compassImg = Image.open("imgs/compass/faceNorth.png").convert("RGBA")
