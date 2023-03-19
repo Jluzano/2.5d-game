db12864d7121c29eb55eb82e7ed7f77cf49ec1f0
Jluzano <justinluzano@gmail.com>
2023-03-19 10:42:52 -0700

    testing tag_raise

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def resizer(e):
     img = img.resize((e.width, e.height), Image.Resampling.LANCZOS)
     img = ImageTk.PhotoImage(img)
     canvas.create_image(0, 0, image = img, anchor="nw")
-    root.Misc.lift(text)
+    canvas.tag_raise(text)
 
 # Initializing the map json file
 m = open("map.json")
@@ -130,7 +130,7 @@ img3 = ImageTk.PhotoImage(img2)
 canvas = Canvas(root, width = 1344, height = 653)
 canvas.pack(fill="both", expand=TRUE)
 canvas.create_image(0, 0, image = img3, anchor="nw")
-text = canvas.create_text(0, 0,fill="red", anchor="nw", font="Times 20 italic bold", text="Hello!")
+text = canvas.create_text(0, 0,fill="red", anchor="ne", font="Times 20 arial", text="Hello!")
 
 # Initiallizing the compass, you start facing north
 compassImg = Image.open("imgs/compass/faceNorth.png").convert("RGBA")
