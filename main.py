30b4ff9ede1380bc9d33584c071a49e1257e57e3
Jluzano <justinluzano@gmail.com>
2023-03-19 10:40:34 -0700

    testing lift method

--- main.py
+++ main.py
@@ -111,11 +111,12 @@ def down(event):
         replace(currentDir, currentPos)
 
 def resizer(e):
-    global img, currentPos, currentDir
+    global img, currentPos, currentDir, text
     img = Image.open(map[currentPos][currentDir]["IMG"])
     img = img.resize((e.width, e.height), Image.Resampling.LANCZOS)
     img = ImageTk.PhotoImage(img)
     canvas.create_image(0, 0, image = img, anchor="nw")
+    root.Misc.lift(text)
 
 # Initializing the map json file
 m = open("map.json")
@@ -129,7 +130,7 @@ img3 = ImageTk.PhotoImage(img2)
 canvas = Canvas(root, width = 1344, height = 653)
 canvas.pack(fill="both", expand=TRUE)
 canvas.create_image(0, 0, image = img3, anchor="nw")
-canvas.create_text(0, 0,fill="red", anchor="nw", font="Times 20 italic bold", text="Hello!")
+text = canvas.create_text(0, 0,fill="red", anchor="nw", font="Times 20 italic bold", text="Hello!")
 
 # Initiallizing the compass, you start facing north
 compassImg = Image.open("imgs/compass/faceNorth.png").convert("RGBA")
